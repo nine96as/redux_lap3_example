dfd3afa54d163a5dc4e3570e2fc4a032ae73f12e
nine96as <ashrafsaid79@gmail.com>
2023-12-12 09:58:15 +0000

    Add useSelector refs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import { faBone } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Doggos } from './pages';
 import './styles/app.css';
 
 function App() {
-  const handleClick = () => console.log('fetching doggos');
+  const loading = useSelector((state) => state.loading);
+  const theDogs = useSelector((state) => state.doggos);
+
+  const handleClick = () => {
+    console.log('fetching doggos');
+  };
 
   return (
     <>
       <h1>These Doggos endorse Redux</h1>
-      <h3 onClick={handleClick} aria-label='refresh' id='refresh'>
+      <h3 onClick={() => handleClick()} aria-label='refresh' id='refresh'>
         fetch! <FontAwesomeIcon icon={faBone} />
       </h3>
       <Doggos />
